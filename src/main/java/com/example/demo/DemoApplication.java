8b875a5980c7d2c82c47d71d76f16c3593ee648f
unknown <U561726@bxo.local>
2023-04-06 11:36:49 +0900

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/api/v1")
+class DemoRestController {
+
+	@GetMapping("/test1")
+	public String test() {
+		return "test ok";
+	}
+
+}
